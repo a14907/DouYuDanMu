fd55e30f99b9f77ffd177adca12689c91d7c8f6b
王德强 <a14907@outlook.com>
2021-01-22 12:30:58 +0800

    更改使用webSocket协议

--- DouYu/Form1.cs
+++ DouYu/Form1.cs
@@ -7,7 +7,9 @@ using System.Drawing;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Net.WebSockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,14 +17,10 @@ namespace DouYu
 {
     public partial class Form1 : Form
     {
-        private IPAddress _serverIP;
-        private int _port = 8601;
-        private Socket _socket;
+        private ClientWebSocket _socket;
         public Form1()
         {
             InitializeComponent();
-            var ips = Dns.GetHostAddresses("openbarrage.douyutv.com");
-            _serverIP = ips[0];
         }
 
         private async void btnGetCate_ClickAsync(object sender, EventArgs e)
@@ -54,10 +52,12 @@ namespace DouYu
                 return;
             }
             tblogs.Clear();
+            StringBuilder sb = new StringBuilder();
             foreach (var item in data.Data)
             {
-                tblogs.AppendText(item.ToString());
+                sb.Append(item.ToString());
             }
+            tblogs.AppendText(sb.ToString());
         }
 
         public void ShowData<T>(ResponseData<T> data)
@@ -88,14 +88,15 @@ namespace DouYu
         private const int MAX_SIZE = 1024 * 1024;
         private byte[] _lenBuff = new byte[4];
         private byte[] _conttentBuff = new byte[MAX_SIZE];
-        private bool isStop = false; 
-        private void btnConnect_Click(object sender, EventArgs e)
-        {
-            _socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
-            _socket.Connect(_serverIP,_port);
+        private bool isStop = false;
 
-            Task.Run((Action)DoWork);
-            Task.Run((Func<Task>)HeartWork);
+        private async void btnConnect_Click(object sender, EventArgs e)
+        {
+            _socket = new ClientWebSocket();
+            await _socket.ConnectAsync(new Uri("wss://danmuproxy.douyu.com:8506/"), CancellationToken.None);
+
+            _ = Task.Run((Action)DoWork);
+            _ = Task.Run((Func<Task>)HeartWork);
             isStop = false;
             btnConnect.Enabled = false;
             btnClose.Enabled = true;
@@ -114,16 +115,16 @@ namespace DouYu
                 }
             };
             var heartbuf = dm.Serialize();
-            
+
             while (!isStop)
             {
-                _socket.Send(heartbuf);
+                await _socket.SendAsync(new ArraySegment<byte>(heartbuf), WebSocketMessageType.Binary, false, CancellationToken.None);
                 tblogs.Clear();
                 await Task.Delay(TimeSpan.FromSeconds(40));
             }
         }
 
-        private void DoWork()
+        private async void DoWork()
         {
             //登陆
             //type@=loginreq/roomid@=301712/
@@ -135,15 +136,15 @@ namespace DouYu
                     { "type","loginreq"},
                     { "roomid",tbRoomId.Text.Trim()}
                 }
-            }; 
+            };
 
             var loginbuf = dm.Serialize();
-            _socket.Send(loginbuf);
+            await _socket.SendAsync(new ArraySegment<byte>(loginbuf), WebSocketMessageType.Binary, false, CancellationToken.None);
             //判断登陆成功
-            ReceiveLenSize(_socket,4,_lenBuff);
+            await ReceiveLenSizeAsync(_socket, 4, _lenBuff);
             var receiveLen = BitConverter.ToInt32(_lenBuff, 0);
             ReceiveContentSize(_socket, receiveLen, _conttentBuff);
-            var resstr=Encoding.UTF8.GetString(_conttentBuff,8,receiveLen-9);
+            var resstr = Encoding.UTF8.GetString(_conttentBuff, 8, receiveLen - 9);
 
             //加入组
             //type@=joingroup/rid@=59872/gid@=0/
@@ -157,20 +158,20 @@ namespace DouYu
                     { "gid",-9999},
                 }
             };
-            _socket.Send(dmJoin.Serialize());
+            await _socket.SendAsync(new ArraySegment<byte>(dmJoin.Serialize()), WebSocketMessageType.Binary, false, CancellationToken.None);
 
             //接受弹幕消息
             while (!isStop)
             {
                 //只抓取弹幕消息，其他消息不管
-                ReceiveLenSize(_socket, 4, _lenBuff);
+                await ReceiveLenSizeAsync(_socket, 4, _lenBuff);
                 receiveLen = BitConverter.ToInt32(_lenBuff, 0);
                 ReceiveContentSize(_socket, receiveLen, _conttentBuff);
                 resstr = Encoding.UTF8.GetString(_conttentBuff, 8, receiveLen - 9);
                 if (resstr.Contains("type@=chatmsg"))
                 {
                     //格式: type@=chatmsg/rid@=301712/gid@=-9999/uid@=123456/nn@=test/txt@=666/level@=1/ 不止这些
-                    var tokens=resstr.Split(new[] { '/'},StringSplitOptions.RemoveEmptyEntries);
+                    var tokens = resstr.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
                     StringBuilder sb = new StringBuilder();
                     string name = "";
                     string level = "";
@@ -183,45 +184,46 @@ namespace DouYu
                         //弹幕文本内容 
                         //level
                         //用户等级
-                        if (token.StartsWith("nn@=") )
+                        if (token.StartsWith("nn@="))
                         {
-                            name=token.Substring(4);
+                            name = token.Substring(4);
                         }
-                        if ( token.StartsWith("txt@=")  )
+                        if (token.StartsWith("txt@="))
                         {
-                            txt=token.Substring(5);
+                            txt = token.Substring(5);
                         }
-                        if ( token.StartsWith("level@="))
+                        if (token.StartsWith("level@="))
                         {
-                            level=token.Substring(7) ;
+                            level = token.Substring(7);
                         }
                     }
                     sb.Append($"{name}({level}):{txt}\r\n\r\n");
-                    tblogs.Invoke((Action<StringBuilder>)(prop=> {
+                    tblogs.Invoke((Action<StringBuilder>)(prop =>
+                    {
                         tblogs.AppendText(sb.ToString());
-                    }),sb);
+                    }), sb);
                 }
             }
         }
 
 
 
-        private void ReceiveLenSize(Socket socket, int size, byte[] buf)
+        private async Task ReceiveLenSizeAsync(WebSocket socket, int size, byte[] buf)
         {
-            var retlen = socket.Receive(buf, size, SocketFlags.None);
-            int sum = retlen;
+            var retlen = await socket.ReceiveAsync(new ArraySegment<byte>(buf, 0, size), CancellationToken.None);
+            int sum = retlen.Count;
             while (sum != size)
             {
-                sum += socket.Receive(buf, sum, size - sum, SocketFlags.None);
+                sum += (await socket.ReceiveAsync(new ArraySegment<byte>(buf, sum, size - sum), CancellationToken.None)).Count;
             }
         }
-        private void ReceiveContentSize(Socket socket, int size, byte[] buf)
-        {            
-            var retlen = socket.Receive(buf, size, SocketFlags.None);
-            int sum = retlen;
+        private async Task ReceiveContentSize(WebSocket socket, int size, byte[] buf)
+        {
+            var retlen = await socket.ReceiveAsync(new ArraySegment<byte>(buf, 0, size), CancellationToken.None);
+            int sum = retlen.Count;
             while (sum != size)
             {
-                sum += socket.Receive(buf, sum, size - sum, SocketFlags.None);
+                sum += (await socket.ReceiveAsync(new ArraySegment<byte>(buf, sum, size - sum), CancellationToken.None)).Count;
             }
         }
 
@@ -240,8 +242,8 @@ namespace DouYu
                     { "type","logout"},
                 }
                 };
-                _socket.Send(dmJoin.Serialize());
-                _socket.Close();
+                await _socket.SendAsync(new ArraySegment<byte>(dmJoin.Serialize()), WebSocketMessageType.Binary, true, CancellationToken.None);
+                await _socket.CloseAsync(WebSocketCloseStatus.Empty, "end", CancellationToken.None);
                 _socket.Dispose();
                 btnConnect.Enabled = true;
                 btnClose.Enabled = false;
